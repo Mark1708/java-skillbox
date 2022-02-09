f888c412003a448c1ce05cbb7b48ad765d5d8ecf
Mark <m.gurianov@mail.ru>
2022-02-09 13:03:14 +0300

    fix

---
+++ 04_05_06_ObjectsAndClasses/Part_3/Basket/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Basket basket = new Basket();
+        basket.add("Milk", 40, 2, 300);
+        basket.add("Oil", 20, 1, 150);
+        basket.print("Milk");
+    }
+}
